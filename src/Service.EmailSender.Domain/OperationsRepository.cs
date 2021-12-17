f2aba1967ffb4925a2b00c9993c4a12ae092a1e2
dmitriy.b <dmitriy.b@smplt.net>
2021-12-17 14:55:08 +0500

    try-catch added

--- src/Service.EmailSender.Domain/OperationsRepository.cs
+++ src/Service.EmailSender.Domain/OperationsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Service.EmailSender.Domain.Models;
 using Service.EmailSender.Postgres;
 
@@ -9,18 +10,30 @@ namespace Service.EmailSender.Domain
 	public class OperationsRepository : IOperationsRepository
 	{
 		private readonly DbContextOptionsBuilder<DatabaseContext> _dbContextOptionsBuilder;
+		private readonly ILogger<OperationsRepository> _logger;
 
-		public OperationsRepository(DbContextOptionsBuilder<DatabaseContext> dbContextOptionsBuilder) => _dbContextOptionsBuilder = dbContextOptionsBuilder;
+		public OperationsRepository(DbContextOptionsBuilder<DatabaseContext> dbContextOptionsBuilder, ILogger<OperationsRepository> logger)
+		{
+			_dbContextOptionsBuilder = dbContextOptionsBuilder;
+			_logger = logger;
+		}
 
 		public async Task Save(string value)
 		{
 			DatabaseContext context = GetContext();
 
-			await context.Operations.AddAsync(new OperationEntity
+			try
 			{
-				Date = DateTime.UtcNow,
-				Value = value
-			});
+				await context.Operations.AddAsync(new OperationEntity
+				{
+					Date = DateTime.UtcNow,
+					Value = value
+				});
+			}
+			catch (Exception exception)
+			{
+				_logger.LogError(exception, exception.Message);
+			}
 
 			await context.SaveChangesAsync();
 		}
